d30d809b21837db30feb2925f3336cb248b214c9
ahmadou5 <78315869+ahmadou5@users.noreply.github.com>
2023-04-15 14:23:49 +0200

    hhh

--- pages/index.js
+++ pages/index.js
@@ -172,7 +172,7 @@ export default function Home() {
 
       checkIfAddressInWhitelist();
       getNumberOfWhitelisted();
-      getListOfAddres();
+      getListOfAddress();
     } catch (err) {
       console.error(err);
     }
